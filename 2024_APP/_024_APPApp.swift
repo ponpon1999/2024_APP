57d0ba9afa5df1e55199befceed9116bf3d5814d
risa <cherry881113@gmail.com>
2024-07-07 00:54:07 +0800

    Initial Commit

---
+++ 2024_APP/_024_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  _024_APPApp.swift
+//  2024_APP
+//
+//  Created by 彭惠暄 on 2024/7/7.
+//
+
+import SwiftUI
+
+@main
+struct _024_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
